b685d5a9bb173c7910ea5d027654d1821cba04d2
MiniPear <subairui@icloud.com>
2022-05-26 09:37:48 +0800

    feat/style (#40)
    * feat(style): outline and notebook
    
    * feat(preview): update style
    
    * docs: update for preview
    
    * chore: update to 0.0.10

--- packages/genji-notebook/public/$genji_stdlib/index.js
+++ packages/genji-notebook/public/$genji_stdlib/index.js
@@ -3,18 +3,24 @@ function fromVue(component) {
   return new Component().$mount().$el;
 }
 
-function preview(thumbnails, options) {
+function preview(thumbnails, options = {}) {
+  const { height = 200, size = "cover" } = options;
   return fromVue({
-    template: `<div class="preview__container">
+    template: `<div class="preview__container" :style="{
+      gridTemplateColumns: 'repeat(auto-fill, minmax(' + height + 'px, 5fr))'
+    }">
         <a v-for="d in thumbnails" :href="d.path">
           <div class="preview__container--image" :style="{
-            backgroundImage: 'url(' + d.thumbnail + ')'
+            backgroundImage: 'url(' + d.thumbnail + ')',
+            backgroundSize: size,
           }"></div>
           <div class="preview__container--title">{{d.title}}</div>
         </a>
     </div>`,
     data() {
       return {
+        height,
+        size,
         thumbnails,
       };
     },
