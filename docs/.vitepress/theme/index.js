b980bf3aed1dab98b02145ddc522aa272d8a0a82
Bairui Su <subairui@icloud.com>
2024-03-24 18:41:56 +0800

    Remove charming (#145)

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -3,7 +3,6 @@ import { h } from "vue";
 import Layout from "genji-theme-vitepress";
 import { dot, barY, plot } from "@observablehq/plot";
 import { json } from "d3-fetch";
-// import { app, range, circle, random, rgb } from "@charming-art/charming";
 import "./custom.css";
 
 function block(color) {
@@ -18,32 +17,19 @@ const props = {
   library: {
     d3: { json },
     Plot: { dot, barY, plot },
-    cm: {
-      // app, range, circle, random, rgb
-    },
     block,
   },
   transform: {
     py(code) {
       return code.replace("print", "new Array");
     },
-    cm(code) {
+    div(code) {
       return `
         call(() => {
-          const app = cm.app({ width: 600, height: 200});
-    
+          const div = document.createElement('div')
           ${code}
-        
-          function dispose(app) {
-            unsubscribe(() => app.dispose());
-          }
-
-          function border(app) {
-            app.node().style.border = "solid #000 1px";
-          }
-    
-          return app.call(dispose).call(border).render().node();
-        })
+          return div;
+        });
       `;
     },
   },
